91600ad81f32b0d5b8aa0df5eda1a7c84d2f94ab
genericname462 <genericname462@users.noreply.github.com>
2015-04-22 14:49:11 +0200

    Implemented iterative jacobi and helper functions

--- main.c
+++ main.c
@@ -1,6 +1,88 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <string.h>
 
+
+int print_matrix(double *matrix, size_t n, size_t m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            printf("%f\t", matrix[i*n + j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int print_vector(double *vector, size_t m) {
+    for (int i = 0; i < m; ++i) {
+        printf("%f ", vector[i]);
+        printf("\n");
+    }
+    return 0;
+}
+
+int scale_matrix(double *matrix, size_t n, size_t m, int scale) {
+    #pragma omp parallel for shared(matrix,n,m)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            matrix[i*n + j] *= scale;
+        }
+    }
+    return 0;
+}
+
+int solve_jacobi(double *matrix, size_t n, size_t m, double *b, double *startvector) {
+    double *x;
+    x = calloc(m, sizeof(double));
+
+    for (int k = 0; k < 20; ++k) {
+        x = memcpy(x, b, m * sizeof(double));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (j != i) {
+                    x[i] = x[i] - matrix[i*n + j] * startvector[j];
+                }
+            }
+            x[i] = x[i] / matrix[i*n + i];
+        }
+        startvector = memcpy(startvector, x, m * sizeof(double));
+        printf("Solution after step %i:\n", k);
+        print_vector(startvector, n);
+    }
+    
+    return 0;
+}
 
 int main() {
+    size_t n, m;
+    n = 2;
+    //m = 2;
+    double *A, *b, *x;
+    A = calloc(n * n, sizeof(double));
+    b = malloc(n * sizeof(double));
+    x = malloc(n * sizeof(double));
+
+    A[0] = 2;
+    A[1] = 1;
+    A[2] = 5;
+    A[3] = 7;
+
+    b[0] = 11;
+    b[1] = 13;
+
+    x[0] = 1;
+    x[1] = 1;
+
+    printf("Matrix A:\n");
+    print_matrix(A, n, n);
+    printf("Vector b:\n");
+    print_vector(b, n);
+    solve_jacobi(A, n, n, b, x);
+    printf("Solution:\n");
+    print_vector(x, n);
+
+
+    free(A); free(b); free(x);
     return 0;
 }
