2716c24d4070e74d170cc64975ddba9a2064f022
genericname462 <genericname462@users.noreply.github.com>
2015-04-23 02:29:33 +0200

    Implemented sharpen function

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 #include <string.h>
 #include <time.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define CLAMP(x, low, high) ({\
+  __typeof__(x) __x = (x); \
+  __typeof__(low) __low = (low);\
+  __typeof__(high) __high = (high);\
+  __x > __high ? __high : (__x < __low ? __low : __x);\
+  })
 
 int print_matrix(double *matrix, size_t n, size_t m) {
     for (int i = 0; i < n; ++i) {
@@ -61,10 +74,10 @@ int solve_jacobi_threaded(double *matrix, size_t n, size_t m, double *b, double 
 
     for (int k = 0; k < 20; ++k) {
         x = memcpy(x, b, m * sizeof(double));
-        int i;
-        #pragma omp parallel for shared(x) private(i)
+        int i, j;
+        #pragma omp parallel for shared(x) private(i,j)
         for (i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
+            for (j = 0; j < n; ++j) {
                 if (j != i) {
                     x[i] = x[i] - matrix[i * n + j] * startvector[j];
                 }
@@ -79,10 +92,85 @@ int solve_jacobi_threaded(double *matrix, size_t n, size_t m, double *b, double 
     return 0;
 }
 
-int main() {
+int sharpen(unsigned char *data, int x, int y, int n){
+    unsigned char *result = malloc((size_t) x * y * n);
+    memcpy(result, data, (size_t) x * y * n);
+    for (int yj = 1; yj < y-1; ++yj) {
+        for (int xi = 1; xi < x-1; ++xi) {
+            for (int c = 0; c < n; ++c) {
+                signed int sum;
+                sum = -1 * (signed int) data[(yj-1) * x * n + (xi-1) * n + c] + -1 * (signed int) data[(yj-1) * x * n + (xi) * n + c] + -1 * (signed int) data[(yj-1) * x * n + (xi+1) * n + c] +
+                       -1 * (signed int) data[(yj) * x * n + (xi-1) * n + c] + 9 * (signed int) data[(yj) * x * n + (xi) * n + c] + -1 * (signed int) data[(yj) * x * n + (xi+1) * n + c] +
+                       -1 * (signed int) data[(yj+1) * x * n + (xi-1) * n + c] + -1 * (signed int) data[(yj+1) * x * n + (xi) * n + c] + -1 * (signed int) data[(yj+1) * x * n + (xi+1) * n + c];
+
+                // Set stuff
+                result[(yj * x * n) + (xi * n) + c] = (unsigned char) CLAMP(sum, 0, 255);
+            }
+        }
+    }
+    memcpy(data, result, (size_t) x * y * n);
+    return 0;
+}
+
+int sharpen2(unsigned char *data, int x, int y, int n){
+    unsigned char *result = malloc((size_t) x * y * n);
+    memcpy(result, data, (size_t) x * y * n);
+    int yj, xi, c;
+    #pragma omp parallel for shared(result) private(yj,xi,c)
+    for (yj = 1; yj < y-1; ++yj) {
+        for (xi = 1; xi < x-1; ++xi) {
+            for (c = 0; c < n; ++c) {
+                // Apply kernel
+                signed int sum;
+                sum = -1 * (signed int) data[(yj-1) * x * n + (xi-1) * n + c] + -1 * (signed int) data[(yj-1) * x * n + (xi) * n + c] + -1 * (signed int) data[(yj-1) * x * n + (xi+1) * n + c] +
+                      -1 * (signed int) data[(yj) * x * n + (xi-1) * n + c] + 9 * (signed int) data[(yj) * x * n + (xi) * n + c] + -1 * (signed int) data[(yj) * x * n + (xi+1) * n + c] +
+                      -1 * (signed int) data[(yj+1) * x * n + (xi-1) * n + c] + -1 * (signed int) data[(yj+1) * x * n + (xi) * n + c] + -1 * (signed int) data[(yj+1) * x * n + (xi+1) * n + c];
+
+                // Set stuff
+                result[(yj * x * n) + (xi * n) + c] = (unsigned char) CLAMP(sum, 0, 255);
+            }
+        }
+    }
+    memcpy(data, result, (size_t) x * y * n);
+    free(result);
+    return 0;
+}
+int image() {
+    int x,y,n;
+    unsigned char *data = stbi_load("Lenna.png", &x, &y, &n, 0);
+    if (data == NULL) {
+        printf("%s\n", stbi_failure_reason());
+        return -1;
+    }
+    printf("x:%i,y:%i,comp:%i\n", x,y,n);
+
+    struct timespec start, finish;
+    double elapsed;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    sharpen(data, x, y, n);
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("sharpen iterative: %f\n", elapsed);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    sharpen2(data, x, y, n);
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("sharpen threaded: %f\n", elapsed);
+
+    if (stbi_write_png("out.png", x, y, n, data, 0) == 0){
+        perror("Error saving file");
+        return -1;
+    }
+    stbi_image_free(data);
+    return 0;
+}
+
+int jac() {
     size_t n, m;
-    n = 4;
-    //m = 2;
+    n = 2;
     double *A, *b, *x;
     A = calloc(n * n, sizeof(double));
     b = malloc(n * sizeof(double));
@@ -95,8 +183,8 @@ int main() {
     A[2] = 5;
     A[3] = 7;
 
-    double C[4][4] = {{1,2,4,7}, {2,4,8,2}, {9,4,8,3}, {1,3,8,7}};
-    double d[4] = {12, 35, 2337, 1632};
+    double C[4][4] = {2,1,5,7};
+    double d[4] = {11, 13};
 
     b[0] = 11;
     b[1] = 13;
@@ -128,4 +216,9 @@ int main() {
 
     free(A); free(b); free(x);
     return 0;
-};
+}
+
+int main(int argc, char *argv[]) {
+    //jac();
+    return image();
+}
