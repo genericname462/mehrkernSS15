bcd834f8c20754ca10787ace26baa98d71918cfb
genericname462 <genericname462@users.noreply.github.com>
2015-04-27 19:31:31 +0200

    First try at the tasks sample

--- openmp/src/async.c
+++ openmp/src/async.c
@@ -1,11 +1,11 @@
 #include "async.h"
 
 int logger(int *run, char *shared_buffer) {
-    while ((*run) < 5) {
+    while ((*run)) {
         printf("Logger: logging... run: %i\n", (*run));
         sleep(2);
         #pragma omp atomic update
-        ++(*run);
+        --(*run);
     }
 
 
@@ -13,10 +13,18 @@ int logger(int *run, char *shared_buffer) {
     return 0;
 }
 
-int user_input(int *run, char *shared_buffer) {
-    while ((*run) < 5) {
-        fgets(shared_buffer, 19, stdin);
-        //printf("user_input: run: %i\n", (*run));
+int user_input(int *run, char *shared_buffer, omp_lock_t lock) {
+    struct pollfd stdin_poll;
+    stdin_poll.fd = STDIN_FILENO;
+    stdin_poll.events = POLLIN | POLLRDBAND | POLLRDNORM | POLLPRI;
+    while ((*run)) {
+        if (poll(&stdin_poll, 1, 1000) == 1) {
+            printf("Got data!\n");
+            omp_set_lock(&lock);
+            fgets(shared_buffer, 19, stdin);
+            omp_unset_lock(&lock);
+        }
+        printf("user_input: run: %i\n", (*run));
     }
 
 
@@ -24,9 +32,11 @@ int user_input(int *run, char *shared_buffer) {
     return 0;
 }
 
-int printer(int *run, char *shared_buffer) {
-    while ((*run) < 5) {
+int printer(int *run, char *shared_buffer, omp_lock_t lock) {
+    while ((*run)) {
+        omp_set_lock(&lock);
         printf("Printer: run: %i %s\n", (*run), shared_buffer);
+        omp_unset_lock(&lock);
         sleep(2);
     }
 
@@ -37,8 +47,10 @@ int printer(int *run, char *shared_buffer) {
 
 int async_demo(int num_threads) {
 
-    int run = 1;
+    int run = 5;
     char *shared_buffer = calloc(20 , 1);
+    omp_lock_t bufferlock;
+    omp_init_lock(&bufferlock);
 
     #pragma omp parallel shared(run)
     {
@@ -46,7 +58,8 @@ int async_demo(int num_threads) {
         {
             #pragma omp task shared(run, shared_buffer) untied
             {
-                //user_input(&run, shared_buffer);
+
+                user_input(&run, shared_buffer, bufferlock);
             }
             #pragma omp task shared(run, shared_buffer) untied
             {
@@ -54,12 +67,14 @@ int async_demo(int num_threads) {
             }
             #pragma omp task shared(run, shared_buffer) untied
             {
-                printer(&run, shared_buffer);
+                printer(&run, shared_buffer, bufferlock);
             }
         }
     }
     #pragma omp taskwait
     printf("run final: %i", run);
+    omp_destroy_lock(&bufferlock);
+    free(shared_buffer);
     return 0;
 }
 
--- openmp/src/async.h
+++ openmp/src/async.h
@@ -7,13 +7,14 @@
 #include <time.h>
 #include <stdlib.h>
 #include <sys/unistd.h>
+#include <poll.h>
 
 
 int logger(int *run, char *shared_buffer);
 
-int user_input(int *run, char *shared_buffer);
+int user_input(int *run, char *shared_buffer, omp_lock_t lock);
 
-int printer(int *run, char *shared_buffer);
+int printer(int *run, char *shared_buffer, omp_lock_t lock);
 
 int async_demo(int num_threads);
 
